f6b18a5fd72e43f3b7eabeb043a01b8fb3209abe
SavourySpringTu <tuxuanbui1111@gmail.com>
2024-06-15 17:19:42 +0700

    dp

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import title from "../styles/title.scss"
+import title from "../styles/title.scss";
 const Title = () => {
-    return (
-        <>
-            <div class="two">
-                <h1>QUẢN LÝ VẬT TƯ
-                    <span>by Savoury Spring Tu1</span>
-                </h1>
-            </div>
-
-        </>
-    )
-}
+  return (
+    <>
+      <div class="two">
+        <h1>
+          QUẢN LÝ VẬT TƯ
+          <span>by Savoury Spring Tu12</span>
+        </h1>
+      </div>
+    </>
+  );
+};
 export default Title;
