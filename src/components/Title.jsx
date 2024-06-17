8d8f367487334f4de8aa65dab4063cfffb500406
SavourySpringTu <tuxuanbui1111@gmail.com>
2024-06-17 15:03:08 +0700

    xong ne

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,7 +6,7 @@ const Title = () => {
       <div class="two">
         <h1>
           QUẢN LÝ VẬT TƯ
-          <span>by Savoury Spring Tu12</span>
+          <span>by Savoury Spring Tu</span>
         </h1>
       </div>
     </>
